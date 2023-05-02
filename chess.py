d24793071880d869a6dbe0b687aac7d38eaaafa3
Erik P <122167559+EriktheRDev@users.noreply.github.com>
2023-05-02 13:15:06 -0400

    Added Collision Check [Rooks]

--- chess.py
+++ chess.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 
 #sg.Popup("CHESS PLAYGROUND")
 
-print("chess playground log")
+print("chess playground log")   
 
 class Chess_Piece:
     all_pieces = {} #{Piece Name:Piece Position}
@@ -28,13 +28,16 @@ class Rook(Chess_Piece):
                 if i == col_y:
                     legal_row = i
         #initializing the piece, globally
-        if self.color == "Black":
-            self.all_pieces[self.color + "Rook" + self.black_rook_list[0]] = self.pos
-            del self.black_rook_list[0]
-        elif self.color == "White":
-            self.all_pieces[self.color + "Rook" + self.white_rook_list[0]] = self.pos
-            del self.white_rook_list[0]
-        self.global_name = self.color + "Rook" + self.white_rook_list[0]
+        if self.color + "Rook" + self.black_rook_list[0] in self.all_pieces:
+            pass
+        else:
+            if self.color == "Black":
+                self.all_pieces[self.color + "Rook" + self.black_rook_list[0]] = self.pos
+                del self.black_rook_list[0]
+            elif self.color == "White":
+                self.all_pieces[self.color + "Rook" + self.white_rook_list[0]] = self.pos
+                del self.white_rook_list[0]
+            self.global_name = self.color + "Rook" + self.white_rook_list[0]
         #legalizing all columns + rows
         pos_x = -1
         pos_y = -1
@@ -46,7 +49,7 @@ class Rook(Chess_Piece):
             legal_moves.append(str(columns2[pos_y]) + str(legal_row))
         pos = 0
         for i in legal_moves:
-            if i in self.all_pieces.values():
+            if i in self.all_pieces.values() and i != self.pos:
                 legal_moves[pos] = str(legal_moves[pos][0]) + "x" + str(legal_moves[pos][1])
             else:
                 pos += 1
@@ -59,13 +62,49 @@ class Rook(Chess_Piece):
                 continue
             else:
                 pos += 1
-    def move(self, col_x, col_y):
-        pass
-
+    def legal_move_update(self):
+        col_x, col_y = self.col_x, self.col_y #Refreshed COL_X and COL_Y
+        legal_moves = [] #Reset Legal Moves
+        columns = 8
+        columns2 = ["A","B","C","D","E","F","G","H"]
+        rows2 = [1,2,3,4,5,6,7,8]
+        rows = 8
+        if col_x in columns2:
+            for i in columns2:
+                if i == col_x:
+                    legal_column = i
+        if col_y in rows2:
+            for i in rows2:
+                if i == col_y:
+                    legal_row = i
+        #legalizing all columns + rows
+        pos_x = -1
+        pos_y = -1
+        for i in rows2:
+            pos_x += 1
+            legal_moves.append(str(legal_column) + str(rows2[pos_x]))
+        for i in columns2:
+            pos_y += 1
+            legal_moves.append(str(columns2[pos_y]) + str(legal_row))
+        pos = 0
+        for i in legal_moves:
+            if i in self.all_pieces.values() and i != self.pos:
+                legal_moves[pos] = str(legal_moves[pos][0]) + "x" + str(legal_moves[pos][1])
+            else:
+                pos += 1
+        self.legal_moves = legal_moves
+        pos = 0
+        for i in self.legal_moves:
+            if i == self.pos:
+                del self.legal_moves[pos]
+                pos += 1
+                continue
+            else:
+                pos += 1
+            #This works well! :D
 
 rook_test_mark = Rook("B", 3, "Black")
-rook_test_mark2 = Rook("B", 6, "White")
-print(rook_test_mark.all_pieces)
-#print(rook_test_mark.pos, rook_test_mark.legal_moves)
+rook_test_mark2 = Rook("C", 3, "White")
+rook_test_mark.legal_move_update()
 print(rook_test_mark.all_pieces)
 print(rook_test_mark.global_name, rook_test_mark.pos, rook_test_mark.legal_moves)
